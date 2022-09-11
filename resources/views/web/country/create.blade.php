08734fc9b52670122932ed869ccd6d6094030b7f
Eivydas123 <eivydasvickus@gmail.com>
2022-09-11 17:45:45 +0300

    first commit

---
+++ resources/views/web/country/create.blade.php
@@ -0,0 +1,7 @@
+
+<select class="form-select">
+    <option selected>select country</option>
+    <option value="1">One</option>
+    <option value="2">Two</option>
+    <option value="3">Three</option>
+</select>
